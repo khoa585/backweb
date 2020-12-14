f7a75c20652415d18f26c4c0db7910015b4a6f23
khoa585 <hoduykhoa2k@gmail.com>
2020-12-14 11:43:42 +0700

    first commit

--- src/Crawler/getList.js
+++ src/Crawler/getList.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const request = require("request-promise");
 let cheerio = require("cheerio");
 const URL_PAGE = "http://truyenqq.com/";
-
+let commicDb = require("../Modal/comic");
 router.get("/test", async () => {
     let url = URL_PAGE;
     let resultData = await request(url);
@@ -11,15 +11,20 @@ router.get("/test", async () => {
     let load = $('.story-item');
     let listLink = [];
     load.each(function (i, element) {
-
-        // let linkCommic1 = cheerio.load(element)("div.top-notice > span.time-ago").text()
         let linkCommic1 = cheerio.load(element)("a:first-child").attr("href")
     
         if (linkCommic1) {
             listLink.push(linkCommic1);
         }
     })
-    console.log(listLink)
+    let listPromise = listLink.map(item=>addCommic(item));
+    let dataPromise = await Promise.all(listPromise);
+
+    return dataPromise.length ;
+
 }
 );
+const addCommic = (Link)=>{
+    return commicDb.create({url:Link});
+}
 export default router;
